5b20f84182360b2d6c9df401d5143c872825cbbe
safe user <safeuser@yawhide.com>
2016-12-14 02:14:49 -0500

    ?

--- redditSuggest/scripts/save-comments.js
+++ redditSuggest/scripts/save-comments.js
@@ -1,21 +1,25 @@
 const async = require('async');
 const fs = require('fs');
+const log = require('better-logs')('save-comment-script');
 const reddit = require('../models/reddit');
 
 const saveFilePath = 'comment_file_path.txt';
 const redditPrefixLen = 'https://www.reddit.com/'.length;
 
 let curFilePath = fs.readFileSync(saveFilePath, 'utf8');
+log.debug('curFilePath:', curFilePath);
 
-
-fs.readdirSync('comments/', (err, filePaths) => {
+fs.readdir('comments/', (err, filePaths) => {
+  if (err) log.error(err);
   async.eachLimit(filePaths, 1, (filePath, eachLimitCB) => {
+    log.debug(filePath)
     if (filePath !== curFilePath) return;
     fs.writeFileSync(saveFilePath, filePath);
 
     let body = JSON.parse(fs.readFileSync(filePath));
     body.forEach(commentInfo => {
       let redditUrl = commentInfo.redditUrl.substring(redditPrefixLen);
+      log.debug('cur redditUrl:', redditUrl, filePath);
       reddit.getSomeComments(redditUrl, eachLimitCB);
     });
   });
